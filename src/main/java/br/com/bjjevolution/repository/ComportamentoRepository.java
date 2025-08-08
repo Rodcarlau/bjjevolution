2b00b2aef923f14f4d5cefb5eb3610b286e184c9
RodrigoBill <ramosrodrigo7@hotmail.com>
2025-08-08 08:31:14 -0300

    Configuração Login Google

--- src/main/java/br/com/bjjevolution/repository/ComportamentoRepository.java
+++ src/main/java/br/com/bjjevolution/repository/ComportamentoRepository.java
@@ -1,9 +1,11 @@
 package br.com.bjjevolution.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.bjjevolution.model.Comportamento;
 
+@Repository
 interface ComportamentoRepository extends JpaRepository<Comportamento, Long> {
     
 }
